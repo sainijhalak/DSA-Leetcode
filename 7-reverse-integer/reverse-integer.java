99fa10216890ad5e94ddd57c7fdd19ce02b2cc0e
sainijhalak <156598369+sainijhalak@users.noreply.github.com>
2025-09-18 19:34:17 +0530

    Time: 1 ms (81.27%) | Memory: 41.1 MB (43.92%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int num=x;
+        int s=0;
+        while(num>0||num<0){
+            int r=num%10;
+           
+            if (s > Integer.MAX_VALUE / 10 || (s == Integer.MAX_VALUE / 10 && r > 7)) return 0;
+if (s < Integer.MIN_VALUE / 10 || (s == Integer.MIN_VALUE / 10 && r < -8)) return 0;
+ s=s*10+r;
+             num=num/10;
+        }
+        return s;
+    }
+}
