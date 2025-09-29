d2c3f4899f68e4ffeb98362642efdc51679ea1c1
sainijhalak <156598369+sainijhalak@users.noreply.github.com>
2025-09-29 23:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (91.73%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin =celsius + 273.15;
+        double Fahrenheit = celsius * 1.80 + 32.00;
+        double []arr=new double[2];
+        arr[0]=kelvin;
+        arr[1]=Fahrenheit;
+        return arr;
+
+    }
+}
