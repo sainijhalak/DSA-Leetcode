56af4ead939ea15ffcb357dee89d0753f8c048df
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2025-12-06 17:07:40 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (46.59%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,17 +1,34 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-      int evenCount = 0;
-        for(int i:nums){
-            if(i % 2==0) evenCount++;
+    
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            nums[i]=0;
         }
-        int i = 0;
-        for(i = 0; i < evenCount; i++){
-            nums[i] = 0;
+        else{
+            nums[i]=1;
         }
-        while(i < nums.length){
-            nums[i++] = 1;
+    }
+    int j=0;
+    int k=nums.length-1;
+    while(j<k){
+        if(nums[j]!=0 && nums[k]==0){
+            int temp=nums[j];
+            nums[j]=nums[k];
+            nums[k]=temp;
+            k--;
         }
+        else if(nums[j]!=0 && nums[k]!=0){
+            k--;
+        }
+        else if(nums[j]==0){
+            j++;
+        }
+        else{
+            break;
+        }
+    }
+    return nums;
 
-        return nums;
     }
 }
