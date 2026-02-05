d30031e3757507f71f2d9cfd28c220b627fecf74
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-02-06 00:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (16.50%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,20 +1,6 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int os=0;
-        int es=0;
-        int i=2;
-      while(n>0){
-os=os+i-1;
-es=es+i;
-i=i+2;
-n--;
-      }
-      int min=Math.min(os,es);
-      for(int j=min;j>0;j--){
-        if(es%j==0 && os%j==0){
-            return j;
-        }
-      }
-      return 0;
+       
+      return n;
     }
 }
