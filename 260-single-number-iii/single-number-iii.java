aae4e9bc1496787c73207bed6f84a39b8a1217cb
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-02-23 23:01:21 +0530

    Time: 7 ms (30.87%) | Memory: 48.2 MB (38.87%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Map <Integer,Integer> map =new HashMap<>();
+for(int i=0;i<nums.length;i++){
+    map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+}
+int []ans=new int[2];
+int i=0;
+for (int key : map.keySet()) {
+    if(map.get(key)==1){
+       ans[i]= key;
+       i++;
+    }
+}
+    return ans; 
+    }
+}
