31aafedca1576563153a6e8fa0f8915f6bcd9e7d
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-06-19 21:08:44 +0530

    Time: 55 ms (63.13%) | Memory: 49.8 MB (25.17%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,25 +1,32 @@
 class Solution {
-    Boolean isvowel(char c){
-        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    boolean isvowel(char c){
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
     }
-    public String sortVowels(String s) {
-        List<Character> list=new ArrayList<>();
-        for(int i=0;i<s.length();i++){
-            if(isvowel(s.charAt(i))) list.add(s.charAt(i));
-        }
-        Collections.sort(list);
-        String ans="";
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            if(isvowel(s.charAt(i))){
-                ans=ans+list.get(j);
-                j++;
-            }
-            else{
-                ans=ans+s.charAt(i);
-            }
 
+    public String sortVowels(String s) {
+        List<Character> list = new ArrayList<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(isvowel(s.charAt(i))) {
+                list.add(s.charAt(i));
+            }
         }
-        return ans;
+
+        Collections.sort(list);
+
+        StringBuilder ans = new StringBuilder();
+        int j = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(isvowel(s.charAt(i))){
+                ans.append(list.get(j));
+                j++;
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+
+        return ans.toString();
     }
 }
