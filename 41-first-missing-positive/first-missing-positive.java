aa534bed622539c969d5b9113ecd1fb1d11675ce
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-10 20:32:48 +0530

    Time: 1 ms (100.00%) | Memory: 71.2 MB (59.23%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,30 +1,17 @@
 class Solution {
-    boolean lessThanOne(int []nums){
-        boolean ans=true;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0) ans=false;
-        }
-        return ans;
-    }
     public int firstMissingPositive(int[] nums) {
-       
-       Set<Integer> set = new TreeSet<>();
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                set.add(nums[i]);
+            while(nums[i]>0 && (nums[i]-1)<nums.length && nums[nums[i]-1]!=nums[i]){
+                int temp=nums[nums[i]-1];
+                nums[nums[i]-1]=nums[i];
+                nums[i]=temp;
             }
-            
         }
-    
-       if(lessThanOne(nums)) return 1; 
-        int n=1;
-        int i=0;
-       for(int x: set){
-        if(x!=n) return n;
-        n++;
-        i++;
-       }
-       if(i==set.size()) return n;
-    return 0;
+int j=0;
+        while(j<nums.length){
+            if(nums[j]!=j+1) return j+1;
+            j++;
+        }
+       return nums.length+1;
     }
 }
