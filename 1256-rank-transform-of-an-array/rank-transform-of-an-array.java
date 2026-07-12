20730919a5f641a458a64bbe0b0aaccb4c47fdea
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-12 16:27:45 +0530

    Time: 30 ms (71.06%) | Memory: 75.5 MB (57.67%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] help=new int[arr.length];
+        for(int i =0;i<help.length;i++){
+            help[i]=arr[i];
+        }
+        Arrays.sort(arr);
+        Map <Integer,Integer> map=new HashMap<>();
+        int m=1;
+        for(int i=0;i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],m);
+                m++;
+            }
+        }
+        int[]ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=map.get(help[i]);
+        }
+        return ans;
+    }
+}
