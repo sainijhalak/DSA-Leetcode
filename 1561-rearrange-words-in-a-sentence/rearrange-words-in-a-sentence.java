d389de7fe319eb22f6a082ba1fec6044ecbb7968
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-22 21:52:52 +0530

    Time: 24 ms (61.64%) | Memory: 48 MB (16.98%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
@@ -1,22 +1,30 @@
 class Solution {
     public String arrangeWords(String text) {
-        text=Character.toLowerCase(text.charAt(0))+text.substring(1,text.length());
-     TreeMap<Integer, List<String>> map = new TreeMap<>();
-       String[]arr=text.trim().split(" ");
-       for(int i=0;i<arr.length;i++){
-        if(!map.containsKey(arr[i].length())){
-           map.put(arr[i].length(),new ArrayList<>());
+        text = Character.toLowerCase(text.charAt(0)) + text.substring(1);
+
+        TreeMap<Integer, List<String>> map = new TreeMap<>();
+
+        String[] arr = text.trim().split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i].length())) {
+                map.put(arr[i].length(), new ArrayList<>());
+            }
+            map.get(arr[i].length()).add(arr[i]);
         }
-        map.get(arr[i].length()).add(arr[i]);
-       }
-       String ans="";
-       for(int x: map.keySet()){
-              List<String> l=map.get(x);
-              for(int j=0;j<l.size();j++){
-                ans=ans+l.get(j)+" ";
-              }
-       }
-       ans=Character.toUpperCase(ans.charAt(0))+ans.substring(1,ans.length());
-       return ans.trim();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int x : map.keySet()) {
+            List<String> l = map.get(x);
+            for (int j = 0; j < l.size(); j++) {
+                sb.append(l.get(j)).append(" ");
+            }
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+
+        return sb.toString();
     }
 }
