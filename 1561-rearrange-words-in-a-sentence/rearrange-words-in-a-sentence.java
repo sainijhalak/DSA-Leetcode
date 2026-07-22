92c0b28d44ff585fb183ae262b320cf439e1fafe
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-22 21:49:24 +0530

    Time: 74 ms (12.58%) | Memory: 55.7 MB (5.66%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
@@ -1,7 +1,7 @@
 class Solution {
     public String arrangeWords(String text) {
         text=Character.toLowerCase(text.charAt(0))+text.substring(1,text.length());
-       Map<Integer,List<String>> map=new HashMap<>();
+     TreeMap<Integer, List<String>> map = new TreeMap<>();
        String[]arr=text.trim().split(" ");
        for(int i=0;i<arr.length;i++){
         if(!map.containsKey(arr[i].length())){
@@ -9,11 +9,9 @@ class Solution {
         }
         map.get(arr[i].length()).add(arr[i]);
        }
-       List<Integer> list=new ArrayList<>(map.keySet());
-       Collections.sort(list);
        String ans="";
-       for(int i=0;i<list.size();i++){
-              List<String> l=map.get(list.get(i));
+       for(int x: map.keySet()){
+              List<String> l=map.get(x);
               for(int j=0;j<l.size();j++){
                 ans=ans+l.get(j)+" ";
               }
