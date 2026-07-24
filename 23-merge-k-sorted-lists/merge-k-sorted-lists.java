35f459a80509f38ff1418f0f4e42b1b1d1a660e3
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-24 23:25:32 +0530

    Time: 9 ms (17.36%) | Memory: 46.7 MB (63.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,6 +1,6 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        // Step 1: Saare values uthao
+       
         List<Integer> values = new ArrayList<>();
         
         for (ListNode list : lists) {
@@ -11,10 +11,10 @@ class Solution {
             }
         }
         
-        // Step 2: Sort karo
+        
         Collections.sort(values);
         
-        // Step 3: Nayi list banao
+      
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         
