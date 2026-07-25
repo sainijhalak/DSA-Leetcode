7116cf960b1f798b7042c5829b795a018ee3e379
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-07-25 20:03:27 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (49.11%) - LeetSync

--- 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
@@ -1,22 +1,15 @@
 class Solution {
     public int[] evenOddBit(int n) {
-        String binary = Integer.toBinaryString(n);
-        char [] ans=binary.toCharArray();
-        int k=0;
-        int l=ans.length-1;
-        while(k<l){
-            char temp=ans[k];
-            ans[k]=ans[l];
-            ans[l]=temp;
-            k++;
-            l--;
+        int even = 0, odd = 0;
+        int pos = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) { 
+                if (pos % 2 == 0) even++;
+                     else odd++;
+            }
+            n =n>> 1; 
+            pos++;
         }
-        int e=0;
-        int o=0;
-        for(int i=0;i<ans.length;i++){
-            if(i%2==0 && ans[i]=='1') e++;
-            else if(i%2!=0 && ans[i]=='1') o++;
-        }
-        return new int[]{e,o};
+        return new int[]{even, odd};
     }
 }
