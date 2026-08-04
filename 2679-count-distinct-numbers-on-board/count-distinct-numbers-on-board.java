16e53e9395b425dc88d67b90018c39e2b230403f
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-08-04 17:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.83%) - LeetSync

--- 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.java
+++ 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.java
@@ -1,15 +1,6 @@
 class Solution {
     public int distinctIntegers(int n) {
-        Set<Integer> set=new HashSet<>();
-        while(n>0){
-              int num=n;
-              int i=1;
-              while(i<num){
-                if(num%i==1) set.add(i);
-                i++;
-              }
-             n--;
-        }
-        return set.size()+1;
+        int ans=(n==1)?1:n-1;
+        return ans;
     }
 }
