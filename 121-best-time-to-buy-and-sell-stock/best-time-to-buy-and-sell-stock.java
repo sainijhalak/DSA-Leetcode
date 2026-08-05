21cfceb4ac659aefbad5c8fa734460b3c1932d7f
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-08-05 21:11:44 +0530

    Time: 2 ms (41.97%) | Memory: 94.3 MB (86.41%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int pro=0;
-        int i=0;
-        int j=1;
-        int min=prices[i];
+        int min=prices[0];
         int ans=0;
-        while(i<prices.length && j<prices.length){
-            if(prices[j]<min){
-                i=j;
-                min=prices[j];
-            }
-            else{
-              int c=prices[j]-min;
-              ans=Math.max(ans,c);
-            }
-             j++;
+        for(int i=1;i<prices.length;i++){
+            int n=prices[i]-min;
+            ans=Math.max(ans,n);
+            min=Math.min(min,prices[i]);
         }
         return ans;
     }
