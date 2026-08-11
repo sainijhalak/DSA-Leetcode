624ceb37b23b9ecfb91e550dc00af1848e038eca
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-08-11 23:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (71.52%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -1,63 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     static int ans;
 
-    int minnikalladle(TreeNode temp, int min) {
-        if (temp == null) return min;
-
-        min = Math.min(min, temp.val);
-
-        int leftMin = minnikalladle(temp.left, min);
-        int rightMin = minnikalladle(temp.right, min);
-
-        return Math.min(min, Math.min(leftMin, rightMin));
+    void optimalHojaSimSim(TreeNode root,int min,int max){
+        if(root==null) return;
+        min=Math.min(min,root.val);
+        max=Math.max(max,root.val);
+        optimalHojaSimSim(root.left,min,max);
+        optimalHojaSimSim(root.right,min,max);
+        int mi=Math.abs(root.val-min);
+        int ma=Math.abs(root.val-max);
+        ans=Math.max(ans,Math.max(mi,ma));
     }
-
-    int maxnikalladle(TreeNode temp, int max) {
-        if (temp == null) return max;
-
-        max = Math.max(max, temp.val);
-
-        int leftMax = maxnikalladle(temp.left, max);
-        int rightMax = maxnikalladle(temp.right, max);
-
-        return Math.max(max, Math.max(leftMax, rightMax));
-    }
-
-    void Retrying(TreeNode root) {
-        if (root == null) return;
-
-        int leftmin;
-        int rightmin;
-        int leftmax;
-        int rightmax;
-
-        if (root.left != null) {
-            leftmin = minnikalladle(root.left, root.left.val);
-            leftmax = maxnikalladle(root.left, root.left.val);
-
-            int l1 = Math.abs(root.val - leftmin);
-            int l2 = Math.abs(root.val - leftmax);
-
-            ans = Math.max(ans, Math.max(l1, l2));
-        }
-
-        if (root.right != null) {
-            rightmin = minnikalladle(root.right, root.right.val);
-            rightmax = maxnikalladle(root.right, root.right.val);
-
-            int r1 = Math.abs(root.val - rightmin);
-            int r2 = Math.abs(root.val - rightmax);
-
-            ans = Math.max(ans, Math.max(r1, r2));
-        }
-
-        Retrying(root.left);
-        Retrying(root.right);
-    }
-
     public int maxAncestorDiff(TreeNode root) {
-        ans = 0;
-        Retrying(root);
+        ans=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        optimalHojaSimSim(root,min,max);
         return ans;
     }
 }
