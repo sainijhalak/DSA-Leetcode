46a7e7737699a2445f604772231119f06552e173
Jhalak Saini <156598369+sainijhalak@users.noreply.github.com>
2026-09-03 09:23:52 +0530

    Time: 150 ms (35.80%) | Memory: 41.6 MB (99.92%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,28 @@
 class Solution {
-    boolean dfs(char [][]board,int i,int j,int[][]vis,String word,int idx){
-         if(idx==word.length()) return true;
-         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(idx) || vis[i][j]==1){
-            return false;
-         }
-         vis[i][j]=1;
-         boolean found=dfs(board,i+1,j,vis,word,idx+1) || 
-         dfs(board,i-1,j,vis,word,idx+1) || dfs(board,i,j+1,vis,word,idx+1) || dfs(board,i,j-1,vis,word,idx+1);
+    static boolean ans;
+    void wordKoKroSearch(char[][]board,int[][]vis,String word,int i,int j,int k){
+        if(k==word.length()) {
+            ans=true;
+            return;
+        }
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || vis[i][j]==1 || board[i][j]!=word.charAt(k)) return;
 
-          vis[i][j]=0;
-        return found;
+        vis[i][j]=1;
+        wordKoKroSearch(board,vis,word,i-1,j,k+1);
+        wordKoKroSearch(board,vis,word,i+1,j,k+1);
+        wordKoKroSearch(board,vis,word,i,j-1,k+1);
+        wordKoKroSearch(board,vis,word,i,j+1,k+1);
+        vis[i][j]=0;
     }
     public boolean exist(char[][] board, String word) {
-        boolean ans=false;
+        ans=false;
         int n=board.length;
         int m=board[0].length;
-        int[][] vis=new int[n][m];
+        int[][]vis=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word.charAt(0)){
-
-                    ans=dfs(board,i,j,vis,word,0);
-
-                    if(ans==true) return true;
+                    wordKoKroSearch(board,vis,word,i,j,0);
                 }
             }
         }
